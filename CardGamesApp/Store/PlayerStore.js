27f7d5ca564c976160faf5f06ec2be8dfd80feb9
brisfors <brisfors@kth.se>
2018-05-07 16:03:51 +0200

    more changes for multiplayer

--- CardGamesApp/Store/PlayerStore.js
+++ CardGamesApp/Store/PlayerStore.js
@@ -1,6 +1,8 @@
 import { EventEmitter } from 'events';
 import dispatcher from '../Dispatcher/Dispatcher';
 import {Player} from '../Components/PlayerObjects';
+net = require('net');
+
 //class for storing all players
 class PlayerStore extends EventEmitter{
   constructor(){
@@ -17,6 +19,25 @@ class PlayerStore extends EventEmitter{
     this.players.push(player);
     this.emit("Change");
   }
+
+//someone sent you a player and you want to do 2 things:
+//1: check if they have your IP address, since you can't know yourself yet
+//2: check if this new player is not you, and in that case make sure you don't have them added yet.
+  addPlayerRemote(player){
+    let player = player;
+    if(player.getId() == this.me.getId() && player.getAddress() != this.me.getAddress()){
+      this.me.setAddress(player.getAddress());
+      return;
+    }
+    if(!this.containsAddress(player.getAddress()){
+      socket = new net.Socket();
+      socket.connect(9000, player.getAddress());
+      player.setConnection(socket);
+      this.players.push(player);
+      this.emit("Change");
+    }
+  }
+
 //removing a player: iterate through array til id matches then remove and move array to fill
   removePlayer(id){
     for (var i = 0; i < this.players.length; i++) {
@@ -42,6 +63,10 @@ class PlayerStore extends EventEmitter{
     switch(action.type){
       case "ADD_PLAYER": {
         this.addPlayer(action.player);
+        break;
+      }
+      case "ADD_PLAYER_REMOTE": {
+        this.addPlayerRemote(action.player);
         break;
       }
       case "REMOVE_PLAYER": {
